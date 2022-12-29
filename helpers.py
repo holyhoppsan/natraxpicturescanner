94ad80835338181f2fcfe5bbd0a598b4edae00e7
Daniel Hall <daniel.hall@live.se>
2022-12-29 19:56:59 +0100

    Added for getting the contours in an api version compatible way and an algorithm for auto_canny

--- helpers.py
+++ helpers.py
@@ -20,3 +20,34 @@ def resize(image, output_width=None, output_height=None, inter=cv2.INTER_AREA):
     resized_image = cv2.resize(image, dimensions, interpolation=inter)
 
     return resized_image
+
+
+def grab_contours(contours):
+    if len(contours) == 2:
+        contours = contours[0]
+
+    elif len(contours) == 3:
+        contours = contours[1]
+
+    else:
+        raise Exception(
+            (
+                "Contours tuple must have length 2 or 3, "
+                "otherwise OpenCV changed their cv2.findContours return "
+                "signature yet again. Refer to OpenCV's documentation "
+                "in that case"
+            )
+        )
+
+    return contours
+
+
+def auto_canny(image, sigma=0.33):
+    # compute the median of the single channel pixel intensities
+    v = np.median(image)
+    # apply automatic Canny edge detection using the computed median
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    edged = cv2.Canny(image, lower, upper)
+    # return the edged image
+    return edged
