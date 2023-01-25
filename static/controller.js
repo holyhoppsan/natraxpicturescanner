f3c1d5df9523fbdf90576eebb4785671099c7c07
Daniel Hall <daniel.hall@live.se>
2023-01-25 22:57:28 +0000

    Setup of template with call to controller script

---
+++ static/controller.js
@@ -0,0 +1,16 @@
+var buttonCapture = document.getElementById("capture");
+
+buttonCapture.onclick = function () {
+    alert("Hello")
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            alert(xhr.responseText);
+            var image = document.getElementById("image");
+            image.src = "/image_viewer?" + new Date().getTime();
+        }
+    }
+    xhr.open("POST", "/capture_status");
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.send(JSON.stringify({ status: "true" }));
+};
